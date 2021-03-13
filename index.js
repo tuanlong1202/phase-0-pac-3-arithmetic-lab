cdb001fd7fd05e60e6b84df4a9c60b92c1a7f349
tuanlong1202 <tuanlong1202@gmail.com>
2021-03-12 23:34:48 -0500

    Phase 0 pac 3 arithmetic lab DONE

---
+++ index.js
@@ -0,0 +1,49 @@
+function add () {
+    return ;
+}
+
+function subtract () {
+    return ;
+}
+
+function multiply () {
+    return ;
+}
+
+function divide () {
+    return ;
+}
+
+function add(a,b) {
+    return parseFloat(a + b) ;
+}
+
+function subtract (a,b) {
+    return parseFloat(a - b) ;
+}
+
+function multiply (a,b) {
+    return parseFloat(a*b) ;
+}
+
+function divide (a,b) {
+    return parseFloat(a/b);
+}
+
+function increment(a) {
+    var  i = a ;
+    return i+=1;
+}
+
+function decrement(a) {
+    var d = a ;
+    return d-=1;
+}
+
+function makeInt (n) {
+    return parseInt(n,10) ;
+}
+
+function preserveDecimal (n) {
+    return parseFloat(n);
+}
